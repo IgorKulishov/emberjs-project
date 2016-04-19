5621c3fe58b5c07a2c162d058a86b4492857fb38
Igor Kulishov <igorkulishov@gmail.com>
2016-04-19 06:11:38 -0400

    TEST PROJECT: functionality, forms and styles for List, Detail, Edit, Add links

--- app/components/employees-view-component.js
+++ app/components/employees-view-component.js
@@ -2,15 +2,48 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	employees: [],
+	employeeDetailVisible: false,
+	employeeListVisible: true,
+	employeeDetailEdit: false,
+	employeeDetail : {
+		id: 'null',
+		name: 'null',
+	},
+	employeeDetailToEdit : {},
 	actions: {
-		addNewEmployee: function() {
-			let newEmloyee = this.get('employees');
-			this.get('employees').pushObject({
-				id:(this.get('posts').length + 1),
-				firstName: newEmployee.firstName,
-				lastName: newEmployee.lastName,
-				email: newEmployee.email
-			});
+		showEmployeeDetail: function(item) {
+			this.set('employeeDetailVisible', true);
+			this.set('employeeListVisible', false);
+			this.set('employeeDetail', item);
+		},
+		showEmployeeList: function() {
+			this.set('employeeDetailVisible', false);
+			this.set('employeeListVisible', true);
+		},
+		employeeDetailEditForm: function(item) {
+			this.set('employeeDetailVisible', false);
+			this.set('detailEditVisible', true);
+			this.set('employeeDetailToEdit', item);
+
+		},
+		saveDetailEdit: function(item) {
+			this.set('detailEditVisible', false);
+			this.set('employeeListVisible', true);			
+		},
+		addNewEmployee: function(item) {
+			this.set('employeeListVisible', true);
+			this.set('addNewEmployeeVisible', false);
+			item.id = this.get('employees').length + 1;
+			this.get('employees').pushObject(item);			
+		},
+		cancelDetail: function() {
+			this.set('detailEditVisible', false);
+			this.set('employeeListVisible', true);
+			this.set('addNewEmployeeVisible', false);
+		},
+		addNewEmployeeForm: function() {
+			this.set('employeeListVisible', false);
+			this.set('addNewEmployeeVisible', true);
 		}
 	}
 });
