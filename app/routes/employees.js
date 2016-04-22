820a4ab86eac36d01a5776dad460797d984805dd
Igor Kulishov <igorkulishov@gmail.com>
2016-04-22 07:53:18 -0400

    TEST PROJECT: added mirage for local instance

--- app/routes/employees.js
+++ app/routes/employees.js
@@ -3,13 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model: function() {
 		//gets default data 
-		return $.get("employees.json");
+		//return $.get("employees.json");
+		return this.store.findAll('employee');
 	},
 	actions: {
 		employeeTheDetail: function(newEployeeDetail) {
 			console.log(newEployeeDetail);
 			//call some api
-			return $.post("api/employees", newEployeeDetail);
+			//return $.post("api/employees", newEployeeDetail);
+			return this.store.createRecord('employee', newEployeeDetail);
 		}
 	}
 });
